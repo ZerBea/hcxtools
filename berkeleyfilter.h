9bb2856efe119c89e66cebfadd7523d66be977fa
ZerBea <ZeroBeat@gmx.de>
2017-08-24 20:43:04 +0200

    added information about hard- and soft-coded BPF

--- berkeleyfilter.h
+++ berkeleyfilter.h
@@ -1,6 +1,6 @@
 /*===========================================================================*/
-
-/* Berkeleyfilter - Blacklist */
+/* Berkeley Packet Filter (BPF) - Blacklist */
+/* pay attention: the hard-coded BPF is used by wlandump-ng, wlanresponse and wlancap2hcx */
 
 const char *filterstring = "!(wlan host 00:00:00:00:00:00 || wlan src 00:00:00:00:00:00)";
 
