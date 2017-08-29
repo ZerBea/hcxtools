bb99fed74d969b503f11b75452c1a6e49df25a58
Alex Stanev <alex@stanev.org>
2017-08-29 19:53:52 +0300

    avoid warning

--- wlanhc2hcx.c
+++ wlanhc2hcx.c
@@ -139,7 +139,7 @@ for(p = 0; p < hcsize; p++)
 		zeiger++;
 		continue;
 		}
-	if((zeiger->eapol_size < 8) || (zeiger->eapol_size > sizeof(zeiger->eapol) -4))
+	if((zeiger->eapol_size < 8) || (zeiger->eapol_size > 256 -4))
 		{
 		zeiger++;
 		eapolerror++;
