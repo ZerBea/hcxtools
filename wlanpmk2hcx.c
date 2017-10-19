295ec7361d03f4e996f17f1195b92fe21e778ce1
magnum <john.magnum@hushmail.com>
2017-10-20 00:06:47 +0200

    Avoid build warnings 'warning: value computed is not used [-Wunused-value]' from OpenSSL BIO stuff. Purely cosmetical.

--- wlanpmk2hcx.c
+++ wlanpmk2hcx.c
@@ -35,9 +35,9 @@ bio = BIO_push(b64, bio);
 
 BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL);
 BIO_write(bio, buffer, length);
-BIO_flush(bio);
+(void) BIO_flush(bio);
 BIO_get_mem_ptr(bio, &bufferPtr);
-BIO_set_close(bio, BIO_NOCLOSE);
+(void) BIO_set_close(bio, BIO_NOCLOSE);
 BIO_free_all(bio);
 *b64text=(*bufferPtr).data;
 return;
