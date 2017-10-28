6cde851489b9e4408631e81acff211c515f4aed5
Rosen Penev <rosenp@gmail.com>
2017-10-28 16:20:07 -0700

    Fix -Wundef

--- common.h
+++ common.h
@@ -1,7 +1,7 @@
 #include <stddef.h>
 #include "berkeleyfilter.h"
 
-#if __APPLE__
+#ifdef __APPLE__
 #define __BYTE_ORDER BYTE_ORDER
 #define __BIG_ENDIAN BIG_ENDIAN
 #define __LITTLE_ENDIAN LITTLE_ENDIAN
@@ -743,7 +743,7 @@ struct hcxhrc
 };
 typedef struct hcxhrc hcxhrc_t;
 
-#if __APPLE__
+#ifdef __APPLE__
 #define be64toh(n) (((n) << 56) | (((n) & 0xff00) << 40) | (((n) & 0xff0000) << 24) | (((n) & 0xff000000) << 8) | (((n) >> 8) & 0xff000000) | (((n) >> 24) & 0xff0000) | (((n) >> 40) & 0xff00) | ((n) >> 56))
 #endif
 
