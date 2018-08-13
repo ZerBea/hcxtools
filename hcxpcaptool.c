0949f9e5cfd2c80f3d0e5bd6eb24c862f5dcbdef
ZerBea <ZeroBeat@gmx.de>
2018-08-13 23:08:37 +0200

    add support for WPA-EAP-TLS PMKIDs

--- hcxpcaptool.c
+++ hcxpcaptool.c
@@ -1233,7 +1233,7 @@ uint8_t essidold[32];
 essidchangecount = 0;
 qsort(apstaessidliste, apstaessidcount, APSTAESSIDLIST_SIZE, sort_apstaessidlist_by_ap_essid);
 
-if(hcpmkidaoutname != NULL)
+if((apstaessidliste != NULL) && (hcpmkidaoutname != NULL))
 	{
 	if((fhoutlist = fopen(hcpmkidaoutname, "a+")) != NULL)
 		{
@@ -4507,7 +4507,7 @@ if(handshakeliste != NULL)
 	outputwpalists(pcapinname);
 	}
 
-if((apstaessidliste != NULL) && (pmkidliste != NULL))
+if(pmkidliste != NULL)
 	{
 	outputpmkidlists();
 	}
