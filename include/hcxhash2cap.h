3d0cdc89ab2188568211a2bc6dc54285ef0b5305
ZeroBeat <ZeroBeat@gmx.de>
2019-02-23 01:05:55 +0100

    more ENDIAN fixes

--- include/hcxhash2cap.h
+++ include/hcxhash2cap.h
@@ -9,3 +9,16 @@
 #define HCXP_JOHN			'4'
 #define HCXP_HELP			'h'
 #define HCXP_VERSION			'v'
+
+#ifdef __BYTE_ORDER__
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#define BIG_ENDIAN_HOST
+#endif
+#else
+#ifdef __OpenBSD__
+# include <endian.h>
+# if BYTE_ORDER == BIG_ENDIAN
+#   define BIG_ENDIAN_HOST
+# endif
+#endif
+#endif
