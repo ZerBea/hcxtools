13d0a71f84c656beaf96a94a6f7e4ef2f37135bd
ZeroBeat <ZeroBeat@gmx.de>
2019-08-23 23:56:55 +0200

    added weak candidate based on wpa-sec analysis

--- hcxpsktool.c
+++ hcxpsktool.c
@@ -527,11 +527,19 @@ static void writeessidadd(FILE *fhout, char *essid)
 int c, d;
 static char essidstring[PSKSTRING_LEN_MAX +PSKSTRING_LEN_MAX +PSKSTRING_LEN_MAX] = {};
 
-
 for(c = 22222; c <= 99999; c += 11111)
 	{
 	snprintf(essidstring, PSKSTRING_LEN_MAX +PSKSTRING_LEN_MAX , "%s%d", essid, c);
 	writepsk(fhout, essidstring);
+	}
+
+if(essidglen <= 2)
+	{
+	for(c = thisyear +1; c < 1000000; c++)
+		{
+		snprintf(essidstring, PSKSTRING_LEN_MAX +PSKSTRING_LEN_MAX , "%s%d", essid, c);
+		writepsk(fhout, essidstring);
+		}
 	}
 
 if(essidglen <= 3)
