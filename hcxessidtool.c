a83ae3aa11ed2fdb2b0bf2b368df7a2aff6efca1
ZerBea <ZeroBeat@gmx.de>
2019-09-13 11:29:21 +0200

    do the same on PMKIDs

--- hcxessidtool.c
+++ hcxessidtool.c
@@ -142,7 +142,7 @@ for(zeiger = pmkidlist; zeiger < (pmkidlist +pmkidcount); zeiger++)
 		}
 	else
 		{
-		if((memcmp(zeigernext->macap, zeiger->macap, 6) != 0) && (memcmp(zeigernext->essid, zeiger->essid, zeiger->essidlen) != 0))
+		if((memcmp(zeigernext->macap, zeiger->macap, 6) != 0) || (memcmp(zeigernext->essid, zeiger->essid, zeiger->essidlen) != 0))
 			{
 			fwriteaddr1(zeiger->macap, fh_file); 
 			fwriteessidstr(zeiger->essidlen, zeiger->essid, fh_file); 
