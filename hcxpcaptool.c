e23aaada70362d8dd1c9f7fefcabdec4f02ea507
ZerBea <ZeroBeat@gmx.de>
2019-09-19 10:39:19 +0200

    some code cleanups

--- hcxpcaptool.c
+++ hcxpcaptool.c
@@ -927,27 +927,35 @@ return false;
 /*===========================================================================*/
 int omac1_aes_128_vector(const uint8_t *key, size_t num_elem, const uint8_t *addr[], const size_t *len, uint8_t *mac)
 {
-	CMAC_CTX *ctx;
-	int ret = -1;
-	size_t outlen, i;
+CMAC_CTX *ctx;
+int ret = -1;
+size_t outlen, i;
 
-	ctx = CMAC_CTX_new();
-	if (ctx == NULL)
-		return -1;
-
-	if (!CMAC_Init(ctx, key, 16, EVP_aes_128_cbc(), NULL))
-		goto fail;
-	for (i = 0; i < num_elem; i++) {
-		if (!CMAC_Update(ctx, addr[i], len[i]))
-			goto fail;
+ctx = CMAC_CTX_new();
+if (ctx == NULL)
+	{
+	return -1;
 	}
-	if (!CMAC_Final(ctx, mac, &outlen) || outlen != 16)
+if (!CMAC_Init(ctx, key, 16, EVP_aes_128_cbc(), NULL))
+	{
+	goto fail;
+	}
+for (i = 0; i < num_elem; i++)
+	{
+	if (!CMAC_Update(ctx, addr[i], len[i]))
+		{
 		goto fail;
+		}
+	}
+if (!CMAC_Final(ctx, mac, &outlen) || outlen != 16)
+	{
+	goto fail;
+	}
 
-	ret = 0;
+ret = 0;
 fail:
-	CMAC_CTX_free(ctx);
-	return ret;
+CMAC_CTX_free(ctx);
+return ret;
 }
 /*===========================================================================*/
 int omac1_aes_128(const uint8_t *key, const uint8_t *data, size_t data_len, uint8_t *mac)
