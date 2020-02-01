d4b54afc853c6eecf7ef6723a9fda770a65efea0
ZeroBeat <ZeroBeat@gmx.de>
2020-02-01 11:31:13 +0100

    fixed typo

--- hcxpcapngtool.c
+++ hcxpcapngtool.c
@@ -559,7 +559,7 @@ if((proberequestcount +authenticationcount +associationrequestcount +reassociati
 	{
 	printf("\nWarning:\n"
 		"This dump file contains no important frames like\n"
-		"proberequest, authentication, association or reassociation\n"
+		"proberequest, authentication, association or reassociation!\n"
 		"That makes it hard to retrieve the PSK!\n");
 	}
 
