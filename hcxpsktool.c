705a1fc7a560620a34f7b0f624a440ef7f680160
ZeroBeat <ZeroBeat@gmx.de>
2020-07-09 18:31:09 +0200

    added weak candidate based on wpa-sec analysis

--- hcxpsktool.c
+++ hcxpsktool.c
@@ -279,7 +279,7 @@ static const char *secondword[] =
 "kayak", "key", "kite", "knight",
 "ladder", "lake", "lawn", "leader", "lemon", "light", "lion", "lotus",
 "mango", "mesa", "mint", "mirror", "mobile", "month", "moon", "museum",
-"nest", "nwagon",
+"nest", "noble",
 "oasis", "object", "oboe", "ocean", "octopus", "onion", "owl", "oxygen",
 "panda", "pant", "parade", "park", "pear", "pencil", "phoenix", "piano", "planet", "pledge", "plum", "pocket", "pond",
 "poodle", "potato", "prairie", "prose",
