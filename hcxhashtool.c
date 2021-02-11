f2142c7189752466135e68de273a580fb43a3977
ZeroBeat <ZeroBeat@gmx.de>
2021-02-11 18:50:21 +0100

    cosmetics

--- hcxhashtool.c
+++ hcxhashtool.c
@@ -602,7 +602,7 @@ if(pkey == NULL)
 	EVP_MD_CTX_free(mdctx);
 	return;
 	}
-if(EVP_DigestSignInit(mdctx, NULL,  EVP_sha1(), NULL, pkey) != 1)
+if(EVP_DigestSignInit(mdctx, NULL, EVP_sha1(), NULL, pkey) != 1)
 	{
 	EVP_PKEY_free(pkey);
 	EVP_MD_CTX_free(mdctx);
