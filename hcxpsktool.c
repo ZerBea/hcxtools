4d5b1844ce3e7f7a3f63fbc88bc794f2e9901063
ZerBea <ZeroBeat@gmx.de>
2021-11-14 19:18:53 +0100

    added weak candidate based on wpa-sec analysis

--- hcxpsktool.c
+++ hcxpsktool.c
@@ -2182,13 +2182,18 @@ static void preparebssid(FILE *fhout, unsigned long long int macaddr)
 static int c;
 static unsigned long long int oui;
 static unsigned long long int nic;
-unsigned int nici;
+static unsigned int nici;
 static int swap;
 static int me;
 static char pskstring[PSKSTRING_LEN_MAX] = {};
 
 nici = ~macaddr &0xffffff;
 fprintf(fhout, "wlan%06x\n", nici);
+nici = ~macaddr &0xffffffff;
+fprintf(fhout, "%08x\n", nici);
+nici = (~macaddr >> 8) &0xffffffff;
+fprintf(fhout, "%08x\n", nici);
+
 me = macaddr &0xffffff;
 fprintf(fhout, "05%6d\n", me);
 oui = macaddr &0xffffff000000L;
